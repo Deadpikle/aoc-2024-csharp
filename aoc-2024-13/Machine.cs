75b80aad5a93e3e9c409647377bb372847a5ac11
Michael Babienco <mbabienco@southern.edu>
2024-12-14 21:40:32 +0900

    Solution to Day 13 pt 2

--- aoc-2024-13/Machine.cs
+++ aoc-2024-13/Machine.cs
@@ -1,33 +1,34 @@
 ﻿using System.Numerics;
+using System.Runtime.InteropServices;
 
 class Machine
 {
     public Button A;
     public Button B;
-    public BigInteger PrizeX;
-    public BigInteger PrizeY;
+    public long PrizeX;
+    public long PrizeY;
 
     public const int CostA = 3;
     public const int CostB = 1;
 
     private bool IsPart2 = false;
 
-    public Machine(Button a, Button b, BigInteger x, BigInteger y, bool isPart2 = false)
+    public Machine(Button a, Button b, long x, long y, bool isPart2 = false)
     {
         A = a;
         B = b;
         PrizeX = x + (isPart2 ? 10000000000000 : 0);
         PrizeY = y + (isPart2 ? 10000000000000 : 0);
         SolutionCache = (-1, -1);
-        dict = new Dictionary<(BigInteger, BigInteger), bool>();
+        dict = new Dictionary<(long, long), bool>();
         IsPart2 = isPart2;
     }
 
-    private (BigInteger, BigInteger) SolutionCache;
+    private (long, long) SolutionCache;
 
-    private Dictionary<(BigInteger, BigInteger), bool> dict;
+    private Dictionary<(long, long), bool> dict;
 
-    private (BigInteger, BigInteger) CheckSolution(BigInteger currATimes, BigInteger currBTimes)
+    private (long, long) CheckSolution(long currATimes, long currBTimes)
     {
         // Use dict for cache as we will hit same cases multiple times
         if (dict.ContainsKey((currATimes, currBTimes)))
@@ -84,19 +85,67 @@ class Machine
 
     public bool IsSolvable()
     {
+        return SolveViaMathEquations();
+        //if (SolutionCache.Item1 == -1)
+        //{
+        //    SolutionCache = CheckSolution(0, 0);
+        //}
+        //return SolutionCache.Item1 != -1;
+    }
+
+    public bool SolveViaMathEquations()
+    {
         if (SolutionCache.Item1 == -1)
         {
-            SolutionCache = CheckSolution(0, 0);
+            /*
+            HOW DID I NOT REALIZE THIS IS JUST A SET OF EQUATIONS!!!
+            See: https://www.reddit.com/r/adventofcode/comments/1hd5b6o/2024_day_13_in_the_end_math_reigns_supreme/m1viocb/
+            Button A: X+94, Y+34
+            Button B: X+22, Y+67
+            Prize: X=8400, Y=5400
+            94x + 22y = 8400
+            34x + 67y = 5400
+            now solve the set of equations
+            Ax + By = C
+            Dx + Ey = F
+            y = (C - Ax) / B
+            y = (F - Dx) / E
+            --two y equations equal each other--
+            (C - Ax) / B = (F - Dx) / E
+            BF - BDx = EC - EAx
+            BF - EC - BDx = -EAx
+            BF - EC = BDx - EAx
+            (BF - EC) / (BD - EA) = x
+            can get # for x now and solve for y:
+            y = (c - a * x) / b
+            */
+            decimal a = A.XDelta;
+            decimal b = B.XDelta;
+            decimal c = PrizeX;
+            decimal d = A.YDelta;
+            decimal e = B.YDelta;
+            decimal f = PrizeY;
+            
+            decimal x = (c * e - b * f) / (e * a - b * d);
+            decimal y = (c - a * x) / b;
+            if (x.Scale == 0 && y.Scale == 0)
+            {
+                SolutionCache = ((long)x, (long)y);
+            }
+            else
+            {
+                SolutionCache = (-1, -1);
+            }
         }
         return SolutionCache.Item1 != -1;
     }
 
-    public (BigInteger, BigInteger) GetLeastCostSolution()
+    public (long, long) GetLeastCostSolution()
     {
         return SolutionCache;
     }
 
-    public BigInteger GetLeastCost()
+    public long GetLeastCost()
     {
         return SolutionCache.Item1 * CostA + SolutionCache.Item2 * CostB;
     }
